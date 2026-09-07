c639c7525689749bce096b37a56462e1d1aaaf53
adefemgit <femijnr4@icloud.com>
2026-09-07 13:02:12 +0200

    Fixing the error

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")	
-    assert response.status_code == 999
+    assert response.status_code == 200
 
 def test_health():
     client = app.test_client()
